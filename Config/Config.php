edca825cc2d70bb98fe4de50f83a32bc2936d901
ORESTES0001 <75502802+ORESTES0001@users.noreply.github.com>
2022-11-24 23:29:49 -0300

    Tecno

--- Config/Config.php
+++ Config/Config.php
@@ -20,9 +20,9 @@ const USERTYPE = 7;
 /* ----------------------------------------------------- */
 
 const DB_HOST = "dbtec.mysql.database.azure.com";
-const DB_NAME = "tecnoagro";
-const DB_USER = "root";
-const DB_PASSWORD = "";
+const DB_NAME = "dbtec";
+const DB_USER = "Slender";
+const DB_PASSWORD = "Tecno89!!";
 const DB_CHARSET = "utf8";
 
 /* ----------------------------------------------------- */
