ff78df2f6ca027ffcea5cc70fc882820b7c25032
ORESTES0001 <75502802+ORESTES0001@users.noreply.github.com>
2022-11-25 17:41:46 -0300

    prueba error 3

--- Assets/app/js/Usuarios.js
+++ Assets/app/js/Usuarios.js
@@ -11,7 +11,7 @@ document.addEventListener(
       },
       // consultar los datos a la api
       ajax: {
-        url: `https://localhost/tecnoagro/usuarios/all`,
+        url: `${base_url}/usuarios/all`,
         dataSrc: "",
       },
       // datos desde el servidor
