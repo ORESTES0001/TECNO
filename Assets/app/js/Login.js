09aca555238c1453c14a055d09dac2bd5f24b1e8
ORESTES0001 <75502802+ORESTES0001@users.noreply.github.com>
2022-11-26 01:39:48 -0300

    prueba 31
    prueba 31

--- Assets/app/js/Login.js
+++ Assets/app/js/Login.js
@@ -4,6 +4,41 @@ document.querySelector("#login").addEventListener("submit", function (e) {
   });
   
   async function login() {
-  window.location.href = "https://tecnoagro.azurewebsites.net/Dashboard";
+      
+       debug($controller->$metodo($parametro))  ;
+  let login = document.querySelector("#login");
+    const datos = new FormData(login);
+    try {
+      const url = `${base_url}/Register/ingresar`;
+      const respuesta = await fetch(url, {
+        method: "POST",
+        body: datos,
+      });
+      const resultado = await respuesta.json();
+  
+      if (resultado.error) {
+        new Noty({
+          type: "error",
+          text: `${resultado.error}`,
+          layout: "topCenter",
+          theme: "metroui",
+          timeout: 1500,
+        }).show();
+      } else {      
+          new Noty({
+        type: "success",
+        text: `${resultado.msg}`,
+        layout: "topCenter",
+        theme: "metroui",
+        timeout: 1000,
+      }).show();
+      setTimeout(() =>{
+
+        window.location.href = `${base_url}/dashboard   `;
+      },1500)
+      }
+    } catch (err) {
+      console.log(err);
+    }
   }
   
